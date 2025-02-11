83a4f0b5fa677b0ea84ae4d1e1d60a0383c1ddb5
YEGU KWON <wt2080@gmail.com>
2025-02-11 11:39:34 +0900

    Time: 1187 ms (100%), Space: 160.7 MB (100%) - LeetHub

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.erl
@@ -0,0 +1,9 @@
+-spec min_operations(Nums :: [integer()], K :: integer()) -> integer().
+
+xor_arr([], Prev) -> Prev;
+xor_arr([H | T], Prev) -> xor_arr(T, Prev bxor H).
+
+min_operations(Nums, K) ->
+    length([X || X <- integer_to_list(xor_arr(Nums, K), 2), X =:= $1]).
+
+%    1 bxor 2 bxor 1 bxor 3 bxor 4.

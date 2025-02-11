8933867c61a8f02b1d498d0f9087a0e51afab4e8
YEGU KWON <wt2080@gmail.com>
2025-02-11 11:27:10 +0900

    Time: 0 ms (100%), Space: 7.8 MB (34.2%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
+    unordered_map<int, int> memo{};
+
     int fib(int n) {
         if(n <= 1) return n;
-
-        int a = 0, b = 1;
-        while(n-- > 1){
-            int sum = a + b;
-            a = b;
-            b = sum;
+        if(!memo[n]){
+            memo[n] = fib(n - 1) + fib(n - 2);
         }
-        return b;
+        return memo[n];
     }
 };

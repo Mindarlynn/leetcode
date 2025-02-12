21593ae03688f2f9c50a727a9c377c8b840e208b
Yegu Kwon <wt2080@gmail.com>
2025-02-12 09:50:47 +0900

    Time: 0 ms (100%), Space: 10.2 MB (68.36%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    // 1. transpose matrix
-    // 2. swap ith and N - ith element (flip horizontally)
-
-    void transpose(vector<vector<int>>& matrix){
-        int N = matrix.size();
-        vector<vector<int>> mat_ = matrix;
-
-        for (int i = 0; i < N; ++i)
-            for (int j = 0; j < N; ++j)
-                matrix[i][j] = mat_[j][i];
-    }
-
-    void swap(int& a, int& b){
-        int tmp = a;
-        a = b;
-        b = tmp;
-    }
-
-    void flip(vector<vector<int>>& matrix){
-        int N = matrix.size();
-        
-        for(int i = 0; i < N; ++i)
-            for(int j = 0; j < N / 2; ++j)
-                swap(matrix[i][j], matrix[i][N - j - 1]);   
-    }
-
     void rotate(vector<vector<int>>& matrix) {
-        transpose(matrix);
-        flip(matrix);
+        int n = matrix.size();
+        // transpose
+        for(int i = 0; i < n; ++i){
+            for(int j = i + 1; j < n; ++j){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        
+        // flip horizontally
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n / 2; ++j){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
     }
 };

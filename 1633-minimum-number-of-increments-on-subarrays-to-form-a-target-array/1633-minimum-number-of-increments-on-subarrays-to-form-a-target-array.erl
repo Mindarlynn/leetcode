eb608ab76d04ca3dad190e28ac87eee779158203
Yegu Kwon <wt2080@gmail.com>
2025-02-14 14:04:22 +0900

    Time: 8 ms (100%), Space: 144.5 MB (100%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.erl
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.erl
@@ -1,7 +1,7 @@
 -spec min_number_operations(Target :: [integer()]) -> integer().
 min_number_operations(Target) ->
-    helper([0 | Target]).
-    
+    hd(Target) + helper(Target).
+
 helper([]) -> 0;
 helper([A, B | T]) when B > A -> (B - A) + helper([B | T]);
 helper([H | T]) -> helper(T).

8cde02b49a8c81b6bac21c79a4abd670def1a5a6
Yegu Kwon <wt2080@gmail.com>
2025-02-14 15:59:21 +0900

    Time: 8 ms (100%), Space: 68.7 MB (100%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.erl
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.erl
@@ -5,36 +5,33 @@
 %%                     right = null :: 'null' | #tree_node{}}).
 
 -spec recover_from_preorder(Traversal :: unicode:unicode_binary()) -> #tree_node{} | null.
-recover_from_preorder(<<Traversal/binary>>) ->
-    recover_from_preorder(binary_to_list(Traversal));
-
 recover_from_preorder(Traversal) ->
-    element(1, helper(Traversal)).
+    element(1, helper(binary_to_list(Traversal))).
+
+make_child(Str, Depth) ->
+    case helper(Str, [], 1, Depth + 1) of
+        {null, _} -> {null, Str};
+        Res -> Res
+    end.
 
 helper(Traversal) -> helper(Traversal, [], 0, 0).
 
-helper([], [], _, _) -> {null, []};
-helper([], Tmp, Dashes, Depth) when Dashes =:= Depth -> {#tree_node{val = list_to_integer(Tmp), left = null, right = null}, []};
-helper([], _, _, _) -> {null, []};
+helper([], [], _, _) -> 
+    {null, []};
+helper([], Tmp, D, D) -> 
+    {#tree_node{val = list_to_integer(Tmp), left = null, right = null}, []};
+helper([], _, _, _) -> 
+    {null, []};
 %when dash (-)
 helper([$- | T], Tmp, Dashes, Depth) -> 
     case length(Tmp) of
         0 -> helper(T, Tmp, Dashes + 1, Depth);
         _ when Dashes =/= Depth -> {null, []};
         _ ->  
-            {LeftNode, LeftStr} = 
-                case helper(T, [], 1, Depth + 1) of
-                    {null, _} -> {null, T};
-                    LRes -> LRes
-                end,
-            {RightNode, RightStr} = 
-                case helper(LeftStr, [], 1, Depth + 1) of
-                    {null, _} -> {null, LeftStr};
-                    RRes -> RRes
-                end,
-            %io:format("Val: ~p~nT: ~p~nL: ~p~nR: ~p~n~n", [Tmp, T, LeftStr, RightStr]),
+            {LeftNode, LeftStr} = make_child(T, Depth),
+            {RightNode, RightStr} = make_child(LeftStr, Depth),
             {#tree_node{val = list_to_integer(Tmp), left = LeftNode, right = RightNode}, RightStr}
     end;
 %when number
-helper([H | T], Tmp, Dashes, Depth) when H >= $0 andalso H =< $9 ->
+helper([H | T], Tmp, Dashes, Depth) ->
     helper(T, Tmp ++ [H], Dashes, Depth).

3974f186a781e30933c54687964cef08af991eb5
Yegu Kwon <wt2080@gmail.com>
2025-02-16 11:00:51 +0900

    Time: 1460 ms (0%), Space: 77.4 MB (100%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.erl
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.erl
@@ -1,3 +1,12 @@
+helper([{Idx, H} | _], [{Idx, H} | _]) -> [H * H];
+helper([{FIdx, FVal} | FT], [{RIdx, RVal} | RT]) ->
+    case abs(FVal) < abs(RVal) of
+        true -> helper([{FIdx, FVal} | FT], RT) ++ [RVal * RVal];
+        _ -> helper(FT, [{RIdx, RVal} | RT]) ++ [FVal * FVal]
+    end.
+    
+
 -spec sorted_squares(Nums :: [integer()]) -> [integer()].
 sorted_squares(Nums) ->
-    lists:sort(lists:map(fun(X) -> X * X end, Nums)).
+    Indexed = lists:zip(lists:seq(1, length(Nums)), Nums),
+    helper(Indexed, lists:reverse(Indexed)).

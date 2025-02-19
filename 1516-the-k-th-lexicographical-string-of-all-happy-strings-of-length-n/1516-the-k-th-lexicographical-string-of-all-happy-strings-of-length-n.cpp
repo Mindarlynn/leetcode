dfec41136112d959b67afcceb42a7535e816e39a
Yegu Kwon <wt2080@gmail.com>
2025-02-19 10:43:31 +0900

    Time: 0 ms (100%), Space: 9 MB (79.94%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    void bcktrack(int n, int len, vector<char>& tmp, vector<string>* ans){
+    void bcktrack(int n, int k, int len, int& th, vector<char>& tmp, string* ans){
+        if(th > k) return;
         if(n == len) {
-            ans->push_back(string{tmp.begin(), tmp.end()});
+            if(th == k) (*ans) = string{tmp.begin(), tmp.end()};
+            ++th;
             return;
         }
         for(char i = 'a'; i <= 'c'; ++i){
             if(!tmp.empty() && *tmp.rbegin() == i) continue;
             tmp.push_back(i);
-            bcktrack(n, len + 1, tmp, ans);
+            bcktrack(n, k, len + 1, th, tmp, ans);
             tmp.pop_back();
         }
     }
 
     string getHappyString(int n, int k) {
-        vector<string> res{};
+        string res = "";
         vector<char> tmp;
-        bcktrack(n, 0, tmp, &res);
+        int th = 1;
+        bcktrack(n, k, 0, th, tmp, &res);
 
-        return res.size() < k ? "" : res[k - 1];
+        return res;
     }
 };

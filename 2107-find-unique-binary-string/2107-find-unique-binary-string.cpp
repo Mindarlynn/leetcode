8f883d90d0fc72a06c7ea0382015641e95597cb1
Yegu Kwon <wt2080@gmail.com>
2025-02-20 09:58:15 +0900

    Time: 4 ms (20.82%), Space: 14 MB (21.18%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void bcktrk(const int n, unordered_set<string>& s, string& tmp, string* res){
+        if(tmp.length() > n) return;
+        if(tmp.length() == n){
+            if(!s.contains(tmp)){
+                (*res) = tmp;
+                return;
+            }
+        }
+
+        for(char c = '0'; c <= '1'; ++c){
+            tmp += c;
+            bcktrk(n, s, tmp, res);
+            tmp = tmp.substr(0, tmp.length() - 1);
+            if((*res) != "") return;
+        }
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        const int n = nums.size();
+        unordered_set<string> s{};
+
+        for(int i = 0; i < n; ++i)
+            s.insert(nums[i]);
+
+        string ans, tmp;
+        bcktrk(n, s, tmp, &ans);
+        return ans;        
+    }
+};

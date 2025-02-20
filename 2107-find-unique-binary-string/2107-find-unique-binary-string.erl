3b5ab9b764a5b1fbfb9b476ff8adb80b970e230e
Yegu Kwon <wt2080@gmail.com>
2025-02-20 11:08:37 +0900

    Time: 652 ms (100%), Space: 58.5 MB (100%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.erl
@@ -0,0 +1,21 @@
+-spec find_different_binary_string(Nums :: [unicode:unicode_binary()]) -> unicode:unicode_binary().
+find_different_binary_string(Nums) ->
+    Set = sets:from_list(lists:map(fun binary_to_list/1, Nums)),
+    N = length(Nums),
+    list_to_binary(bcktrk(N, Set, 0, [])).
+
+bcktrk(N, Set, N, Tmp) ->
+    case sets:is_element(Tmp, Set) of
+        true -> [];
+        false -> Tmp
+    end;
+bcktrk(N, Set, Len, Tmp) ->
+    lists:foldl(
+        fun(X, Res) ->
+            _Res = bcktrk(N, Set, Len + 1, Tmp ++ [X]),
+            case _Res of
+                [] -> Res;
+                _ -> _Res
+            end
+        end, [], [$0, $1]
+    ).

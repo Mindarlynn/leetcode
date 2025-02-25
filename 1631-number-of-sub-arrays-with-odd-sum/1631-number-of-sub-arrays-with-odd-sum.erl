c87b9344e8cfe9f6d1391b122770bd532fc5c049
Yegu Kwon <wt2080@gmail.com>
2025-02-25 13:50:24 +0900

    Time: 36 ms (100%), Space: 102.7 MB (100%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.erl
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.erl
@@ -1,14 +1,15 @@
 -spec num_of_subarrays(Arr :: [integer()]) -> integer().
 num_of_subarrays(Arr) ->
+    RemVal = 1000000007,
     element(4, lists:foldl(
         fun(X, {Sum, Odd, Even, Res}) ->
             NewSum = Sum + X,
             {NewOdd, NewEven, NewRes} = 
                 if
                     NewSum rem 2 == 0 ->
-                        {Odd, Even + 1, (Res + Odd) rem 1000000007};
+                        {Odd, Even + 1, (Res + Odd) rem RemVal};
                     true -> 
-                        {Odd + 1, Even, (Res + Even) rem 1000000007}
+                        {Odd + 1, Even, (Res + Even) rem RemVal}
                 end,
             {NewSum, NewOdd, NewEven, NewRes}
         end, {0, 0, 1, 0}, Arr

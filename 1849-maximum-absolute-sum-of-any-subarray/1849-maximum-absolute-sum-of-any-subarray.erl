1ebc7741be31e98ccde609a64ed082432df3110f
Yegu Kwon <wt2080@gmail.com>
2025-02-26 12:20:39 +0900

    Time: 524 ms (100%), Space: 141.2 MB (100%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.erl
@@ -0,0 +1,9 @@
+-spec max_absolute_sum(Nums :: [integer()]) -> integer().
+max_absolute_sum(Nums) ->
+    element(3, lists:foldl(
+        fun(X, {Max, Min, Res}) ->
+            NewMax = max(0, X + Max),
+            NewMin = min(0, X + Min),
+            {NewMax, NewMin, lists:max([NewMax, -NewMin, Res])}
+        end, {0, 0, 0}, Nums
+    )).

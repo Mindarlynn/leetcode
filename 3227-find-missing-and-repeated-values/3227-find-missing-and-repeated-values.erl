cf232d6dd5f350fad5ebb8a50eb82295b76fcef6
Yegu Kwon <wt2080@gmail.com>
2025-03-07 02:56:25 +0900

    Time: 23 ms (100%), Space: 64.3 MB (100%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.erl
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.erl
@@ -3,22 +3,15 @@ freq([H | T]) -> maps:update_with(H, fun(X) -> X + 1 end, 1, freq(T)).
 
 -spec find_missing_and_repeated_values(Grid :: [[integer()]]) -> [integer()].
 find_missing_and_repeated_values(Grid) ->
-    L = lists:flatten(Grid),
-    Freq = freq(L),
-    NN = length(L),
-    Res = lists:foldl(
-        fun(X, Res) ->
+    Flat = lists:flatten(Grid),
+    Freq = freq(Flat),
+    {Dupl, Miss} = lists:foldl(
+        fun(X,  {Dupl, Miss}) ->
             case maps:get(X, Freq, 0) of
-                2 -> Res ++ [X];
-                _ -> Res
+                2 -> {Dupl ++ [X], Miss};
+                0 -> {Dupl, Miss ++ [X]};
+                _ -> {Dupl, Miss}
             end
-        end, [], lists:seq(1, NN)
+        end, {[], []}, lists:seq(1, length(Flat))
     ),
-    lists:foldl(
-        fun(X, Res) ->
-            case maps:get(X, Freq, 0) of
-                0 -> Res ++ [X];
-                _ -> Res
-            end
-        end, Res, lists:seq(1, NN)
-    ).
+    Dupl ++ Miss.

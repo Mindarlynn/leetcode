d8f6c8c30f934231176e6f13fd98181f48fb4e1f
Yegu Kwon <wt2080@gmail.com>
2025-04-06 01:28:11 +0900

    Time: 847 ms (97.58%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,9 +1,7 @@
-select s.student_id, s.student_name, s.subject_name, sum(if(isnull(e.student_id), 0, 1)) as attended_exams
-from (
-    select *
-    from students, subjects
-) s
+select s.student_id, s.student_name, ss.subject_name, sum(if(isnull(e.student_id), 0, 1)) as attended_exams
+from students s
+cross join subjects ss
 left join examinations e
-on s.student_id = e.student_id and s.subject_name = e.subject_name
-group by s.student_id, s.subject_name
+on s.student_id = e.student_id and ss.subject_name = e.subject_name
+group by s.student_id, ss.subject_name
 order by s.student_id;

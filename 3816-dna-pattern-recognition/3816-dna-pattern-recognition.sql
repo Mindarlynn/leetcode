8cbcaaadacf0954f7242699970b4705bae31bfce
Yegu Kwon <wt2080@gmail.com>
2025-04-06 08:25:58 +0900

    Time: 274 ms (95.52%), Space: 0B (100%) - LeetHub

---
+++ 3816-dna-pattern-recognition/3816-dna-pattern-recognition.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select
+    *,
+    if(dna_sequence regexp '^(ATG).*$', 1, 0) as has_start,
+    if(dna_sequence regexp '^.*(TAA|TAG|TGA)$', 1, 0) as has_stop,
+    if(dna_sequence regexp '^.*(ATAT).*$', 1, 0) as has_atat,
+    if(dna_sequence regexp '^.*(G){3,}.*$', 1, 0) as has_ggg
+from samples;

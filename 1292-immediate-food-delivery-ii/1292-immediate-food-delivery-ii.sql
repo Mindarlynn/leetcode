dcd7dc8c5c4355c23243324611b3fb2c62dc8c79
Yegu Kwon <wt2080@gmail.com>
2025-04-06 06:09:17 +0900

    Time: 656 ms (84.61%), Space: 0B (100%) - LeetHub

---
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+with
+    # first orders
+    cte1 as (
+        select distinct(customer_id), min(order_date) as order_date
+        from delivery
+        group by customer_id
+    ),
+    # immediate orders
+    cte2 as (
+        select distinct(customer_id), order_date, 1 as immediate
+        from delivery
+        where order_date = customer_pref_delivery_date
+    )
+
+select round(sum(immediate) / count(*) * 100, 2) as immediate_percentage
+from cte1
+left join cte2
+using(customer_id, order_date)

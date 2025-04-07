58dee5303888f1ebb96618248446032dc41520f8
Yegu Kwon <wt2080@gmail.com>
2025-04-07 18:28:45 +0900

    Time: 478 ms (89.36%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,23 +1,19 @@
 # Write your MySQL query statement below
 
-with 
-    cte1 as (
-        select distinct(product_id), new_price, change_date
+with
+    cte as (
+        select
+            product_id,
+            first_value(new_price) over (partition by product_id order by change_date desc) as price,
+            row_number() over (partition by product_id order by change_date desc) as rn
         from products
         where datediff(date("2019-08-16"), change_date) >= 0
-        order by change_date desc
-    ),
-    cte2 as (
-        select *
-        from cte1
-        group by product_id
-    ),
-    cte3 as (
-        select distinct(product_id)
-        from products
     )
 
-select product_id, ifnull(new_price, 10) as price
-from cte3
-left join cte2
-using(product_id)
+select 
+    unique_product_id.product_id,
+    ifnull(cte.price, 10) as price
+from (select distinct(product_id) from products) as unique_product_id
+left join cte
+on unique_product_id.product_id = cte.product_id
+where cte.rn = 1 or cte.rn is null;

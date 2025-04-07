138669a7a4d58563defea7c90524cdd5af67d878
Yegu Kwon <wt2080@gmail.com>
2025-04-07 18:44:18 +0900

    Time: 533 ms (95.07%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,23 +1,10 @@
-# Write your MySQL query statement below
-
-with cte0 as (
-    select player_id, min(event_date) as event_date
-    from activity
-    group by player_id
-),
-cte1 as (
-    select distinct(a.player_id), 1 as isConsecutive
-    from activity a
-    left join cte0 b
-    on a.player_id = b.player_id and datediff(a.event_date, b.event_date) = 1
-    where b.player_id is not null
-),
-cte2 as (
-    select distinct(player_id)
-    from activity
-)
-
-select ifnull(round(sum(isConsecutive) / count(*), 2), 0) as fraction
-from cte2
-left join cte1
-using(player_id)
+select 
+    round(
+        count(distinct player_id) /
+        (select count(distinct player_id) from activity), 2
+    ) as fraction
+from 
+    activity
+where
+    (player_id, date_sub(event_date, interval 1 day)) in
+    (select player_id, min(event_date) as first_login from activity group by player_id)
